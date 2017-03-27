38d00575cf355c1666435470bc2c9f62c7f31607
Sofía Ignacia Zambrano Mericq <Sofi@wificlient-10-200-3-148.uc.cl>
2017-03-27 14:48:49 -0300

    MigrateDBtoPostreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170327133139) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "usuarios", force: :cascade do |t|
     t.string   "usuario"
     t.string   "nombre"
